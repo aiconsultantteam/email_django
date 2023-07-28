2ec0b674e37b2ea42e9ae66e58aba785318a571d
aiconsultantteam <139223990+aiconsultantteam@users.noreply.github.com>
2023-07-28 17:59:31 +0700

    Update settings.py

--- email_webapp/settings.py
+++ email_webapp/settings.py
@@ -104,9 +104,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://email-app-9a31.onrender.com']
-CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_DOMAIN = 'email-app-9a31.onrender.com'
+CSRF_TRUSTED_ORIGINS = ['email-app-9a31.onrender.com']
+CSRF_COOKIE_SECURE = True
 
 # Add this line
 LOGIN_URL = 'login'
